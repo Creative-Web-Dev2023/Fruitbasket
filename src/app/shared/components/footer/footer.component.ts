c58899a065a1d279ea82e1a913be1459be8d43ff
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-06-21 23:44:23 +0200

    first commit

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styles: [`./footer.component.scss`]
+})
+export class FooterComponent {
+
+}
